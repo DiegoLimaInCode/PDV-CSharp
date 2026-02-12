e969cbbf4f27faae6136015299e376243492a87d
diego <diegolimabarenco@gmail.com>
2026-02-12 00:33:54 -0300

    att de vendaFinal

--- PDVCSharp.WPF/Sections/VendaFinal.xaml.cs
+++ PDVCSharp.WPF/Sections/VendaFinal.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,57 @@ namespace PDVCSharp.WPF.Sections
         public VendaFinal()
         {
             InitializeComponent();
+        }
+
+        private void ValorTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = true;
+
+            if (!char.IsDigit(e.Text, 0))
+                return;
+
+            var textBox = (TextBox)sender;
+            string digits = ExtrairDigitos(textBox.Text) + e.Text;
+            textBox.Text = FormatarValor(digits);
+            textBox.CaretIndex = textBox.Text.Length;
+        }
+
+        private void ValorTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Back || e.Key == Key.Delete)
+            {
+                e.Handled = true;
+
+                var textBox = (TextBox)sender;
+                string digits = ExtrairDigitos(textBox.Text);
+
+                if (digits.Length > 0)
+                    digits = digits[..^1];
+
+                textBox.Text = FormatarValor(digits);
+                textBox.CaretIndex = textBox.Text.Length;
+            }
+        }
+
+        private static string ExtrairDigitos(string texto)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in texto)
+            {
+                if (char.IsDigit(c))
+                    sb.Append(c);
+            }
+            return sb.ToString().TrimStart('0');
+        }
+
+        private static string FormatarValor(string digits)
+        {
+            if (string.IsNullOrEmpty(digits))
+                digits = "0";
+
+            long valor = long.Parse(digits);
+            decimal valorDecimal = valor / 100m;
+            return valorDecimal.ToString("N2", new CultureInfo("pt-BR"));
         }
     }
 }
